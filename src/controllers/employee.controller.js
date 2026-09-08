b4861e9876a17bc370a1505334522b3c536c5e73
kdutta913-ui <kdutta913@gmail.com>
2026-09-08 14:43:44 +0530

    Fixed employee controller and completed remaining CRUD operation

--- src/controllers/employee.controller.js
+++ src/controllers/employee.controller.js
@@ -15,13 +15,96 @@ const createEmployee = async (req,res) => {
         res.status(201).json({message: "Employee created successfully", employee})
     } catch (error) {
         console.log(error)
-        if(error.code === 11000){
-            return res.status(409).json({message:"Email already exists"})
+        if (error.code === 11000) {
+            if (error.keyPattern.email) {
+                return res.status(409).json({message:"Email already exists."})
+            } 
+            else if(error.keyPattern.phoneNum) {
+                return res.status(409).json({message:"Phone number already exists."})
+            }
+        } 
+        else {
+            res.status(500).json({message: "Server error", error:error.message})
         }
-        res.status(500).json({message:"Server error", error: error.message})
+
     }
 };
 
+const getEmployees = async (req,res) =>{
+    try {
+        const employees =  await Employee.find()
+        res.status(200).json({employees})
+        }
+    catch (error) {
+        console.log(error)
+        res.status(500).json({message:"Server error", error:error.message})
+    }
+}
+
+const getEmployeeById = async (req,res)=>{
+    try {
+        const employee = await Employee.findById(req.params.id)
+        if(!employee){
+            return res.status(404).json({message:"Employee doesn't exists"})
+        }
+        res.status(200).json({employee})
+    } catch (error) {
+        console.log(error)
+        if(error.name === "CastError"){
+            return res.status(400).json({message:"Invalid Employee ID."})
+        }
+        res.status(500).json({message:"Server error", error:error.message})
+    }
+}
+
+const updateEmployee = async(req,res)=>{
+    try {
+        const employee = await Employee.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new: true}
+        )
+        if(!employee){
+            return res.status(404).json({message:"Employee doesn't exists."})
+        }
+        res.status(200).json({message:"Employee updated successfully", employee})
+    } catch (error) {
+        console.log(error)
+        if (error.code === 11000) {
+            if (error.keyPattern.email) {
+                return res.status(409).json({message:"Email already exists"})
+            } else if(error.keyPattern.phoneNum){
+                return res.status(409).json({message:"Phone number already exists"})
+            }
+        } 
+        else {
+            res.status(500).json({message:"Server error", error: error.message})
+        }
+    }
+}
+
+const deleteEmployee = async(req,res)=>{
+    try {
+        const employee = await Employee.findByIdAndDelete(
+        req.params.id
+        )
+        if(!employee){
+            return res.status(404).json({message: "Employee doesn't exists."})
+        }
+        res.status(200).json({message:"Deleted successfully", employee})
+    } catch (error) {
+        console.log(error)
+        if(error.name === "CastError"){
+            return res.status(400).json({message:"Invalid Employee ID."})
+        }
+        res.status(500).json({message:"Server error", error: error.message})
+    }
+}
+
 module.exports = {
-    createEmployee
+    createEmployee,
+    getEmployees,
+    getEmployeeById,
+    updateEmployee,
+    deleteEmployee
 };
--- src/routes/employee.routes.js
+++ src/routes/employee.routes.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const { createEmployee } = require('../controllers/employee.controller');
+const { createEmployee, getEmployees, getEmployeeById, updateEmployee, deleteEmployee } = require('../controllers/employee.controller');
 const router = express.Router();
 
 router.post("/", createEmployee) //create employee
-
-router.get("/", (req,res)=>{}) //get all the employees
-router.get("/:id", (req,res)=>{}) // get the employee by id
-router.patch("/:id", (req,res)=>{}) // update something of the employee by id
-router.delete("/:id", (req,res)=>{}) // delete the employee 
+router.get("/", getEmployees) //get all the employees
+router.get("/:id", getEmployeeById) // get the employee by id
+router.patch("/:id", updateEmployee) // update something of the employee by id
+router.delete("/:id", deleteEmployee) // delete the employee 
 
 module.exports = router;
