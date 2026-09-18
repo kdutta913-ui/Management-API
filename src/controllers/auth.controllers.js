be2d5b0b4a839c03a811f71bbf23eb8dffda6c11
kdutta913-ui <kdutta913@gmail.com>
2026-09-18 13:05:28 +0530

    Added login

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,5 +1,15 @@
 const bcrypt = require('bcrypt')
 const User = require("../models/user.models")
+const jwt = require("jsonwebtoken")
+
+const generateToken = (userId, role) =>{
+    return jwt.sign({userId, role},
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "1d"
+        }
+    )
+}
 
 const register = async(req, res, next) =>{
     try {
@@ -40,3 +50,55 @@ const register = async(req, res, next) =>{
         next(error)
     };
 };
+
+//login
+const login = async(req,res,next)=>{
+    try {
+        const {email, password} = req.body;
+        if(!email || !password){
+            return res.status(400).json({message: "Email and passoword are required"})
+        }
+
+        const user = await User.findOne({
+            email
+        });
+        if(!user){
+            return res.status(401).json({message:"Invalid email or password"})
+        }
+
+        if(user.isActive === false){
+            return res.status(401).json({message:"User is not active"})
+        }
+
+        const isPasswordValid = await bcrypt.compare(
+            password,
+            user.password
+        );
+        if(!isPasswordValid){
+            return res.status(401).json({message:"Incorrect password"})
+        }
+
+        const token = generateToken(user._id, user.role);
+
+        res.status(200).json({message:"Login successful",
+            token,
+            user:{
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            }
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+
+
+
+
+module.exports = {
+    register,
+    login
+}
---
+++ src/routes/auth.routes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const { login, register } = require('../controllers/auth.controllers');
+const router = express.Router();
+
+router.post("/login",login)
+router.post("/register", register)
