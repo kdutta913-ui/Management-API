59d61cb2ce415bcebf92d027152fa420230ad913
kdutta913-ui <kdutta913@gmail.com>
2026-09-21 12:28:48 +0530

    Added auth middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const employeeRoute = require("./employee.routes")
+const authRoute = require("./auth.routes")
 
 const router = express.Router();
 
+router.use("/api/auth", authRoute)
 router.use("/api/employees", employeeRoute)
+
 
 module.exports = router;
